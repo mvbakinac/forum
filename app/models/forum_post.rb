6f69bea9820f6ea45d45600c85d977f5f76215d2
Milo Vrach <morelo87@gmail.com>
2017-07-12 10:29:30 +0200

    Add nested resources

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: forum_posts
+#
+#  id              :integer          not null, primary key
+#  user_id         :integer
+#  forum_thread_id :integer
+#  body            :text
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class ForumPost < ApplicationRecord
   belongs_to :user
   belongs_to :forum_thread
